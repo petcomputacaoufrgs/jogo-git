a0000317b4b32401cf352b2aa46e617c5bd75d71
petcomputacaoufrgs <petcompufrgs@gmail.com>
2016-09-13 16:34:59 -0300

    Recursão

--- README.txt
+++ README.txt
@@ -1,7 +1,4 @@
-Vamos falar sobre coisas importantes. Vamos falar do fim do mundo.
-Imagine um cenário nos anos 80 aonde o professor Xavier ainda não é careca, e que está tentando nos convencer
-que James McAvoy se tornará Patrick Stewart ao perder o cabelo.
-É necessário mesmo dizer o quão mal feito o CG daquilo ficou? A história non-sense do Magneto tentando ser
-humanizado ou não, porque francamente não importa tanto. Ou mesmo do Deus Ex Machina que é basicamente o que a Fênix
-se tornou.
-E nem vou falar de Esquadrão Suicida. Isso é assunto para outro commit.
+Vamos brincar de acrônimos recursivos. 
+Em algum momento alguém decidiu que computação poderia ser engraçado e criaram o acrônimo recursivo. 
+É mais fácil mostrar um exemplo pra explicar: BING significa BING Is Not Google. Sério. Isso é um acrônimo recursivo, em que ele é definido a partir dele mesmo. 
+Outros exemplos fantásticos são VISA (VISA International Services Association),   PNG (PNG Not Gif). E claro, LINUX.
--- main.c
+++ main.c
@@ -27,6 +27,127 @@
 
 //Definicoes de estruturas
 
+typedef struct str_pontuacao //Estrutura de pontuacao (salva no arquivo de highscores)
+{
+    float pontuacao;
+    char nome[MAX_NOME];
+
+}PONTUACAO;
+
+typedef struct str_refem //Estrutura usada pelo jogador
+{
+    int localhoriz_ca,localvert_ca,localhoriz_co,localvert_co,localhoriz_ar,localvert_ar,localhoriz_cano,localvert_cano;
+    int vidas;
+    int nivel;
+    PONTUACAO pontuacao;
+    int chave;
+    int condVida;
+    char corpo, cabeca, arma, cano_arma;
+    int tempo_final;
+
+} REFEM;
+
+typedef struct str_tiro //Estrutura do tiro
+{
+    int x,y;
+    char id;
+    int j;
+    float contMovimento; //Tempo para o tiro se mexer
+    int sentido; //Direção que o tiro se move
+    int vidaUtil;
+    struct timeval t0;  // Tempo para o tiro iniciar
+    struct timeval t1;  // Tempo para o tiro mover
+} TIRO;
+
+typedef struct str_inimigo //Estrutura dos inimigos
+{
+    //MOVIMENTO DO INIMIGO
+    char corpo, cabeca, arma, cano_arma;
+    int localhoriz_ca,localvert_ca,localhoriz_co,localvert_co,localhoriz_ar,localvert_ar,localhoriz_cano,localvert_cano;
+    float contador_velocidade_inimigo;
+    float velocidade_inimigo;
+
+    //TIRO DO INIMIGO
+
+    float velocidade_tiro_inimigo;
+    float contador_intervalo_proximo_tiro;
+    float contador_tiro;
+    // VIDA E COLISÃO
+    int vida;
+    int condVida;
+    TIRO tiro_inimigo[MAX_TIRO_INIMIGO];
+
+} INIMIGO;
+
+typedef struct str_chave //Estrutura da chave
+{
+    int local_horiz, local_vert, condExistencia;
+    char cabeca, segredo;
+
+}CHAVE;
+
+//Prototipos das funcoes auxiliares
+
+//Funções fora do jogo
+char menu(int dentro_do_jogo);
+int le_escolha(char choice, int dentro_do_jogo);
+void novo_jogo(int carregado, char nome_do_arquivo[]);
+void imprime_highscore();
+int seleciona_jogos();
+void backstory (int nivel);
+
+//Função jogo
+int jogo(REFEM *jogador, int carregado, char nome_do_arquivo[]);
+void carrega_jogo(char nome_arquivo[], REFEM *jogador, INIMIGO inimigos[]);
+
+//Funções Gráficas do Jogo
+void barra_informacoes(REFEM *jogador, INIMIGO inimigos[], int num_inimigos, float tempo);
+void parede(); //Imprime a parede
+void porta(); //Imprime a porta
+void apaga_vidas(REFEM *jogador);
+void carregando();
+
+//Funcoes estruturais do jogo
+float pontuacao(int nivel, int tempo_final, int num_inimigos);
+int passa_nivel (REFEM *jogador, int tempo_final);
+void reimprime_jogo (INIMIGO inimigo[],TIRO tiro[], int num_inimigos);
+//Funcoes do jogador
+void reseta_jogador(REFEM *jogador);
+char movimento_usuario(REFEM *jogador,CHAVE *chave);
+
+//Funcoes do tiro
+void reseta_tiro (TIRO *tiro, int num_tiro);
+void cria_tiro (TIRO tiro[], REFEM *jogador);
+void movimento_tiro(TIRO *tiro, float velocidade, int atirador, int numero_tiros, float tempo);
+
+//Funcoes do inimigo
+int inicializa_numero_inimigos (int nivel);
+void reseta_inimigos(INIMIGO inimigo[], int *nivel, int num_inimigos);
+void movimento_inimigo(INIMIGO inimigo[], int num_inimigos, float tempo);
+int numero_de_inimigos_mortos ();
+void lugares_indisponiveis (INIMIGO inimigo[], int num_inimigos, int lugares_indisponiveis[]);
+int compara_numeros (int lugar_desejado, int lista[],int num_inimigos, int lugar_do_inimigo_no_arranjo);
+//Função do tiro do inimigo
+void cria_tiro_inimigo (INIMIGO inimigo[],int num_inimigos, float tempo);
+
+//Funcoes de colisão
+int colisao_elastica(TIRO tiro[], int tamanho); //Colisao sem efeitos (bateu na parede)
+int colisao_nao_elastica_jogador(REFEM *jogador, INIMIGO *inimigo); //Colisao com efeitos (bate no jogador)
+int colisao_nao_elastica_inimigo(TIRO tiro[], INIMIGO inimigo[], CHAVE *chave, int num_inimigos); //Colisao com efeitos (bate no inimigo)
+
+//Funções da Chave
+void chave(INIMIGO inimigos[], REFEM *jogador, CHAVE *chave, int num_inimigos, int num_inimigos_mortos);
+void reseta_chave(CHAVE *chave);
+void pega_chave(CHAVE *chave, REFEM *jogador);
+
+//Funcoes de salvar
+int salva_pontuacao(REFEM jogador);
+void leitura_pontuacoes ();
+void salva_jogo(REFEM jogador, INIMIGO inimigos[]);
+void inverte(PONTUACAO pontuacao[], int inicio, int fim);
+int separa_inversao(PONTUACAO pontuacao[], int inicio, int fim);
+int jogos_salvos(char nomes[10][20]);
+
 int main()
 {
 
